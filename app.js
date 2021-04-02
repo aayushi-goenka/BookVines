778acaa1f2e19461cb8b1f8d156decab33ec8c0d
Aayushi Goenka <aayushi.goenka777@gmail.com>
2021-04-02 15:14:32 +0530

    ⚰️ removed redundant code

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const session = require("express-session");
 const passport =require("passport");
 const passportLocalMongoose = require("passport-local-mongoose"); 
 const app = express();
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 //
 const database= require('./database/db')();
@@ -19,23 +22,19 @@ const reviews = require('./database/review');
 
 //Route dependency 
 const loginRout = require('./routes/login')
+const logoutRout = require('./routes/logout')
 const signupRout = require('./routes/signup')
-const creviewRout = require('./routes/creviews')
-const reviewRout = require('./routes/reviews')
 const profileRout = require('./routes/profile')
-const checkoutRout = require('./routes/checkout')
-const addRout = require('./routes/book')
 const productRout = require('./routes/product')
-const genreRout = require('./routes/genre')
 const bookRout=require('./routes/book')
-const bookSearch=require("./routes/")
-
-
+const bookSearch=require("./routes/bookSearch")
+const failRout = require('./routes/fail')
+const landingRout=require('./routes/landing')
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static("public"));
 app.use('/static/', express.static(__dirname + '/static'));
-// app.use('/images/', express.static(__dirname + '/images'));
+
 
 //using cookies
 app.use(session({
@@ -63,54 +62,32 @@ passport.deserializeUser(function(id, done) {
 });
 
 
-
-
-app.get("/", function(req, res){
-  
-    res.render("landing");
-  
-  
-});
+//Landing
+app.use("/",landingRout)
 
 //login
 app.use("/login", loginRout)
 
+//logout
+app.use("/logout", logoutRout)
+
+//fail
+app.use("/fail", failRout)
 
 //sign up
 app.use("/signup", signupRout)
-
-
-//CREVIEWS( ADD OR READ)
-app.use('/creviews', creviewRout)
-
-
-//review
-app.use('/reviews', reviewRout)
-
 
 //profile
 app.use('/profile', profileRout)
 
 
-//chekout
-app.use('/checkout', checkoutRout)
-
-
-//add
-app.use('/add', addRout)
-
-
 //product
 app.use('/product', productRout)
-
-
-//genre
-app.use('/genre', genreRout)
 
 //Book
 app.use('/book',bookRout)
 
 //Book Search
-app.use("/Search",)
+ app.use("/Search",bookSearch)
 
-app.listen(process.env.PORT || 5000,()=>console.log("Up and Running.."))
+app.listen(process.env.PORT || 3000,()=>console.log("Up and Running.."))
